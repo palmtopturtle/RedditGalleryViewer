a41465f9016327d16b0453db7926f7dc29edb184
palmtopturtle <gieske.jeff@gmail.com>
2018-11-05 17:36:18 -0500

    Updates

---
+++ precache-manifest.790c7f375351a822dfec61419cbf03c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8d4fb2a214b64dccc538",
+    "url": "/RedditGalleryViewer/static/js/runtime~main.8d4fb2a2.js"
+  },
+  {
+    "revision": "6fd5071017f5ff84ca0d",
+    "url": "/RedditGalleryViewer/static/js/main.6fd50710.chunk.js"
+  },
+  {
+    "revision": "9b161059d8d3be336001",
+    "url": "/RedditGalleryViewer/static/js/1.9b161059.chunk.js"
+  },
+  {
+    "revision": "6fd5071017f5ff84ca0d",
+    "url": "/RedditGalleryViewer/static/css/main.6d685894.chunk.css"
+  },
+  {
+    "revision": "ebd492a07ef45aa9d438150487957109",
+    "url": "/RedditGalleryViewer/index.html"
+  }
+];
