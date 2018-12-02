011582aed12fb682b4e4bcb68148079963974b4c
palmtopturtle <gieske.jeff@gmail.com>
2018-12-01 19:58:18 -0500

    Moved to separate component

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import styles from "./footer.module.scss";
+
+class Footer extends Component {
+  /**
+   * Handles errors when fetching data, if any occur.
+   */
+  renderError = () => {
+    const { error } = this.props;
+    if (error.message === "Network Error" || error.response.status === 404) {
+      return (
+        <h2>
+          Error: 404 not found! Did you type the subreddit name correctly?
+        </h2>
+      );
+    } else {
+      return <h2>An error occured while fetching data! Is reddit.com down?</h2>;
+    }
+  };
+
+  /**
+   * Displays different information depending on the props.
+   * * isLoading ? shows "Loading..."
+   * * reachedEnd ? informs the user that there are no more posts to load
+   * * error ? runs through error handling
+   * * else, displays "Load More" button
+   */
+  renderFooter = () => {
+    const { isLoading, error, reachedEnd, fetchPosts } = this.props;
+    if (isLoading) {
+      return <h2>Loading...</h2>;
+    } else if (reachedEnd) {
+      return <h2>Reached end!</h2>;
+    } else if (error) {
+      return this.renderError();
+    } else {
+      return (
+        <button
+          className={styles.btn}
+          type="button"
+          name="loadMore"
+          onClick={fetchPosts}
+        >
+          Load More
+        </button>
+      );
+    }
+  };
+
+  render() {
+    return <div className={styles.footer}>{this.renderFooter()}</div>;
+  }
+}
+
+export default Footer;
